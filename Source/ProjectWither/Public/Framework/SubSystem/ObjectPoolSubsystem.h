63a82f8ebfeb991ec4a655c3fb389db8f3b14feb
rabbitmjh <rabbitmjh@naver.com>
2026-08-27 10:55:58 +0900

    feat: 참조 파일들 추가
    Setting, DataAsset, Interface
    Build.cs에 DeveloperSettings 추가

--- Source/ProjectWither/Public/Framework/SubSystem/ObjectPoolSubsystem.h
+++ Source/ProjectWither/Public/Framework/SubSystem/ObjectPoolSubsystem.h
@@ -9,19 +9,17 @@
 
 class UObjectPoolDataAsset;
 
-using FOrderNode =
-TDoubleLinkedList<TObjectPtr<AActor>>::TDoubleLinkedListNode;
+using FOrderNode = TDoubleLinkedList<TObjectPtr<AActor>>::TDoubleLinkedListNode;
 
+// 오브젝트 풀 하나를 나타낼 구조체
 USTRUCT()
 struct FObjectPool
 {
     GENERATED_BODY()
 
     FObjectPool()
-        : ActiveOrderList(
-            MakeShared<TDoubleLinkedList<TObjectPtr<AActor>>>()),
-        ActiveNodeMap(
-            MakeShared<TMap<TObjectPtr<AActor>, FOrderNode*>>())
+        : ActiveOrderList(MakeShared<TDoubleLinkedList<TObjectPtr<AActor>>>()),
+        ActiveNodeMap(MakeShared<TMap<TObjectPtr<AActor>, FOrderNode*>>())
     {
     }
 
@@ -34,12 +32,10 @@ struct FObjectPool
     TSet<TObjectPtr<AActor>> ActiveActors;
 
     // 가장 오래된 활성 Actor를 찾기 위한 순서 목록
-    TSharedPtr<TDoubleLinkedList<TObjectPtr<AActor>>>
-        ActiveOrderList;
+    TSharedPtr<TDoubleLinkedList<TObjectPtr<AActor>>> ActiveOrderList;
 
     // Actor별 활성 순서 노드
-    TSharedPtr<TMap<TObjectPtr<AActor>, FOrderNode*>>
-        ActiveNodeMap;
+    TSharedPtr<TMap<TObjectPtr<AActor>, FOrderNode*>> ActiveNodeMap;
 
     UPROPERTY(Transient)
     int32 InitialSize = 0;
@@ -52,28 +48,22 @@ struct FObjectPool
 };
 
 UCLASS()
-class PROJECTWITHER_API UObjectPoolSubsystem
-    : public UWorldSubsystem
+class PROJECTWITHER_API UObjectPoolSubsystem : public UWorldSubsystem
 {
     GENERATED_BODY()
 
 public:
-    virtual void Initialize(
-        FSubsystemCollectionBase& Collection) override;
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 
-    virtual void OnWorldBeginPlay(
-        UWorld& InWorld) override;
+    virtual void OnWorldBeginPlay(UWorld& InWorld) override;
 
     virtual void Deinitialize() override;
 
     UFUNCTION(BlueprintCallable)
-    bool RegisterPoolDataAsset(
-        const UObjectPoolDataAsset* InDataAsset,
-        bool bWarmup = false);
+    bool RegisterPoolDataAsset(const UObjectPoolDataAsset* InDataAsset, bool bWarmup = false);
 
     UFUNCTION(BlueprintCallable)
-    bool UnregisterPoolDataAsset(
-        const UObjectPoolDataAsset* InDataAsset);
+    bool UnregisterPoolDataAsset(const UObjectPoolDataAsset* InDataAsset);
 
     UFUNCTION(BlueprintCallable)
     void Warmup(TSubclassOf<AActor> InClass);
@@ -88,35 +78,25 @@ public:
     void ClearAllPools();
 
     UFUNCTION(BlueprintCallable)
-    AActor* Spawn(
-        TSubclassOf<AActor> InClassType,
-        const FTransform& InTransform);
+    AActor* Spawn(TSubclassOf<AActor> InClassType, const FTransform& InTransform);
 
     template<typename T>
-    T* Spawn(
-        TSubclassOf<T> InClassType,
-        const FTransform& InTransform)
+    T* Spawn(TSubclassOf<T> InClassType, const FTransform& InTransform)
     {
-        return Cast<T>(
-            Spawn(
-                TSubclassOf<AActor>(InClassType),
-                InTransform));
+        return Cast<T>(Spawn(TSubclassOf<AActor>(InClassType), InTransform));
     }
 
     template<typename T>
     T* Spawn(const FTransform& InTransform)
     {
-        return Cast<T>(
-            Spawn(T::StaticClass(), InTransform));
+        return Cast<T>(Spawn(T::StaticClass(), InTransform));
     }
 
     UFUNCTION(BlueprintCallable)
     bool ReturnPool(AActor* InActor);
 
 protected:
-    AActor* CreateNewObject(
-        TSubclassOf<AActor> InClassType,
-        const FTransform& InTransform);
+    AActor* CreateNewObject(TSubclassOf<AActor> InClassType, const FTransform& InTransform);
 
     AActor* GetReadyActor(FObjectPool* InPool);
 
