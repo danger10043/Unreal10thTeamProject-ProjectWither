d6668ced1ed8ad1f4254e2bbcd0534e51ce55712
Min Ji Hong <rabbitmjh@naver.com>
2026-09-03 11:29:32 +0900

    feat: TestGroundBoss 생성

---
+++ Source/ProjectWither/Public/BossMonster/GroundBossBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster/MonsterCharacterBase.h"
+#include "GroundBossBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTWITHER_API AGroundBossBase : public AMonsterCharacterBase
+{
+	GENERATED_BODY()
+	
+};
